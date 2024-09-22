25a7a181cd1d1a8fbc50dd01ce87b8a1ae9d3cf8
Andres Ramos <61053256+arght@users.noreply.github.com>
2024-09-22 16:33:15 +0200

    Update conf.py

--- doc/rst/conf.py
+++ doc/rst/conf.py
@@ -91,6 +91,7 @@ html_show_sphinx = False
 html_theme_options = {
     'analytics_id': 'UA-515200-2',  #  Provided by Google in your dashboard
     'description': 'version 1.3.31',
+    'body_max_width': 'none',
     'page_width': 'auto',
     'font_family': 'Georgia'
 }
